e3faa7dace595a33d0e799e0ac8e6c19a7f85955
i507145 <lukas.li@sap.com>
2024-06-10 08:37:50 +0800

    update typings

--- src/index.ts
+++ src/index.ts
@@ -7,51 +7,43 @@ export * from "./generators"
 export * from "./others"
 export * from "./wrappers"
 export * from "./conditionals"
-
-declare global {
   
-  type Pair<K, V> = [key: K, value: V]
+export type Pair<K, V> = [key: K, value: V]
 
-  type Consumer<T> = (v: T) => void
+export type Consumer<T> = (v: T) => void
 
-  type BiConsumer<A, B> = (a: A, b: B) => void
+export type BiConsumer<A, B> = (a: A, b: B) => void
 
-  type TriConsumer<A, B, C> = (a: A, b: B, c: C) => void
+export type TriConsumer<A, B, C> = (a: A, b: B, c: C) => void
 
-  type Func<T, R> = (v: T) => R
+export type Func<T, R> = (v: T) => R
 
-  type BiFunc<A, B, R> = (a: A, b: B) => R
+export type BiFunc<A, B, R> = (a: A, b: B) => R
 
-  type Callback = (...args: any) => any
+export type Callback = (...args: any) => any
 
-  type Supplier<T> = () => T
+export type Supplier<T> = () => T
   
-  type Comparator<T> = (a: T, b: T) => -1 | 0 | 1
+export type Comparator<T> = (a: T, b: T) => -1 | 0 | 1
 
-  type ObjectIndex = string | number
+export type ObjectIndex = string | number
 
-  interface Mapper<A, B> {
-    to?(a: A): B;
-    from?(b: B): A;
-  }
+interface Mapper<A, B> {
+  to?(a: A): B;
+  from?(b: B): A;
+}
 
-  type Bucket<T> = {
-    // (v?: T): T;
-    // (v: (prev: T) => T): T;
-    <U extends T>(v?: T): U;
-    <U extends T>(v: (prev: T) => U): U;
-  }
+export type Bucket<T> = {
+  // (v?: T): T;
+  // (v: (prev: T) => T): T;
+  <U extends T>(v?: T): U;
+  <U extends T>(v: (prev: T) => U): U;
+}
 
-  type StampedBucket<T> = ((updater?: Consumer<T>) => T) & StampedBucketAction<T>;
+export type StampedBucket<T> = ((updater?: Consumer<T>) => T) & StampedBucketAction<T>;
 
-  interface StampedBucketAction<T> {
-    map<O>(call: (v: T) => O): O
-    markChanged(): void
-    reset(v: T): void
-  }
-
-  interface StampedData<T> {
-    data: T
-    timestamp: number
-  }
+interface StampedBucketAction<T> {
+  map<O>(call: (v: T) => O): O
+  markChanged(): void
+  reset(v: T): void
 }
